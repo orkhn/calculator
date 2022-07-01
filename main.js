78544cf758ba38aab7552aa146c4b1b282043bbe
orkhn <orxan.ahadov@gmail.com>
2022-07-02 00:58:06 +0400

    add calculator logic to app UI

--- main.js
+++ main.js
@@ -1,1 +1,125 @@
 "strict mode";
+// SELECTORS
+const lastOperationScreen = document.querySelector(".last-action");
+const currentOperationScreen = document.querySelector(".current-action");
+const clearBtn = document.querySelector("#clear-btn");
+const deleteBtn = document.querySelector("#delete-btn");
+const numBtns = document.querySelectorAll("[data-number]");
+const operatorBtns = document.querySelectorAll("[data-operator]");
+const pointBtn = document.querySelector("#point-btn");
+const equalsBtn = document.querySelector("#equal-btn");
+
+let firstOperand = "";
+let secondOperand = "";
+let currentOperation = null;
+let shouldResetScreen = false;
+
+// EVENT LISTENERS
+window.addEventListener("keydown", handleKeyboardInput);
+equalsBtn.addEventListener("click", evaluate);
+clearBtn.addEventListener("click", clear);
+deleteBtn.addEventListener("click", deleteNumber);
+pointBtn.addEventListener("click", appendPoint);
+
+numBtns.forEach((button) =>
+  button.addEventListener("click", () => appendNumber(button.textContent))
+);
+
+operatorBtns.forEach((button) =>
+  button.addEventListener("click", () => setOperation(button.textContent))
+);
+
+// FUNCTIONS
+function appendNumber(number) {
+  if (currentOperationScreen.textContent === "0" || shouldResetScreen)
+    resetScreen();
+  currentOperationScreen.textContent += number;
+}
+
+function resetScreen() {
+  currentOperationScreen.textContent = "";
+  shouldResetScreen = false;
+}
+
+function clear() {
+  currentOperationScreen.textContent = "0";
+  lastOperationScreen.textContent = "";
+  firstOperand = "";
+  secondOperand = "";
+  currentOperation = null;
+}
+
+function appendPoint() {
+  if (shouldResetScreen) resetScreen();
+  if (currentOperationScreen.textContent === "")
+    currentOperationScreen.textContent = "0";
+  if (currentOperationScreen.textContent.includes(".")) return;
+  currentOperationScreen.textContent += ".";
+}
+
+function deleteNumber() {
+  currentOperationScreen.textContent = currentOperationScreen.textContent
+    .toString()
+    .slice(0, -1);
+}
+
+function setOperation(operator) {
+  if (currentOperation !== null) evaluate();
+  firstOperand = currentOperationScreen.textContent;
+  currentOperation = operator;
+  lastOperationScreen.textContent = `${firstOperand} ${currentOperation}`;
+  shouldResetScreen = true;
+}
+
+function evaluate() {
+  if (currentOperation === null || shouldResetScreen) return;
+  if (currentOperation === "÷" && currentOperationScreen.textContent === "0") {
+    alert("can't divide by zero!");
+    return;
+  }
+  secondOperand = currentOperationScreen.textContent;
+  currentOperationScreen.textContent = roundResult(
+    operate(currentOperation, firstOperand, secondOperand)
+  );
+  lastOperationScreen.textContent = `${firstOperand} ${currentOperation} ${secondOperand} =`;
+  currentOperation = null;
+}
+
+function roundResult(number) {
+  return Math.round(number * 1000) / 1000;
+}
+
+function handleKeyboardInput(e) {
+  if (e.key >= 0 && e.key <= 9) appendNumber(e.key);
+  if (e.key === ".") appendPoint();
+  if (e.key === "=" || e.key === "Enter") evaluate();
+  if (e.key === "Backspace") deleteNumber();
+  if (e.key === "Escape") clear();
+  if (
+    e.key === "+" ||
+    e.key === "-" ||
+    e.key === "*" ||
+    e.key === "/" ||
+    e.key === "%"
+  )
+    setOperation(convertOperator(e.key));
+}
+
+function convertOperator(keyboardOperator) {
+  if (keyboardOperator === "/") return "÷";
+  if (keyboardOperator === "*") return "×";
+  if (keyboardOperator === "-") return "−";
+  if (keyboardOperator === "+") return "+";
+  if (keyboardOperator === "%") return "%";
+}
+
+function operate(operator, a, b) {
+  a = +a;
+  b = +b;
+  if (operator === "+") return a + b;
+  if (operator === "-") return a - b;
+  if (operator === "×") return a * b;
+  if (operator === "÷" && b === 0) return null;
+  if (operator === "÷") return a / b;
+  if (operator === "%" && a !== 0) return (a / 100) * b;
+}
